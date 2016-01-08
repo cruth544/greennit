820456f4e75191d915683b02fb94cf648156f637
Chad Rutherford <c.ruth544@gmail.com>
2016-01-07 18:30:27 -0800

    reset db

--- db/schema.rb
+++ db/schema.rb
@@ -43,8 +43,9 @@ ActiveRecord::Schema.define(version: 20160106012740) do
   create_table "subgreens", force: :cascade do |t|
     t.string   "name"
     t.integer  "admin"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
   create_table "subgreens_users", id: false, force: :cascade do |t|
